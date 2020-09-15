28c9a47540d78a2b6449f75f99ade07e8ffca0b7
faisalsaid <falsdesain@gmail.com>
2020-09-15 22:16:45 +0900

    edit scaleable viewport

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="viewport" content="user-scalable = yes">
 
   <!-- fontawesome -->
   <script src="https://use.fontawesome.com/f3f4d22e3c.js"></script>

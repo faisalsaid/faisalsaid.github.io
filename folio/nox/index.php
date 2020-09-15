d12165e539ead884cae287548b8cbc4562df6f51
faisalsaid <falsdesain@gmail.com>
2020-09-15 21:33:55 +0900

    add folio file

---
+++ folio/nox/index.php
@@ -0,0 +1,356 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <!-- internal css  -->
+  <link rel="stylesheet" href="noxstyle.css" />
+
+  <!-- link google fonts roboto & russo one  -->
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,900;1,300;1,400;1,500;1,900&family=Russo+One&display=swap" rel="stylesheet" />
+  <title>NOX Supplement</title>
+</head>
+
+<body>
+  <header>
+    <div class="container">
+      <nav id="nav-main" class="flex">
+        <img src="nox-logo.svg" alt="" />
+        <a class="nav-menu" href="">HOME</a>
+        <a href="">PRODUCT</a>
+        <a href="">CONTACT</a>
+        <a href="">ABOUT</a>
+        <a href="">BLOG</a>
+        <div class="nav-account float-r">apa</div>
+      </nav>
+      <div class="hero flex">
+        <div class="heroitem">
+          <div class="herocircle flex">
+            <img src="asset/img/nox-product1.png" alt="" />
+          </div>
+        </div>
+        <div class="heroitem">
+          <h2 class="herotitle-one">SPORT</h2>
+          <h2 class="herotitle-two">Supplement</h2>
+          <p class="herodescription">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+            nihil maiores deserunt velit vero suscipit obcaecati culpa
+            tempora? Aliquam esse quos tenetur maxime consectetur nostrum quo
+            dignissimos molestias rem facilis.
+          </p>
+          <button class="btn btn-violet">SHOP NOW</button>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- AUTHENTIVICATION -->
+
+  <main id="auth">
+    <div class="container flex">
+      <div class="auth-item">
+        <p class="form-title">Check Your Product</p>
+        <div class="form-wrap">
+          <form action="" id="auth-form">
+            <input type="text" placeholder="Enter Product Key" />
+            <input type="text" placeholder="exm: 656463" />
+            <input type="text" placeholder="Enter Code Shown Above" />
+            <input class="btn" type="button" value="AUTHENTICATE" />
+          </form>
+        </div>
+      </div>
+      <div class="auth-item">
+        <img src="asset/img/nox-family-product.png" alt="" />
+      </div>
+    </div>
+  </main>
+  <!-- END AUTHENTIVICATION -->
+
+  <!-- BENEFIT -->
+  <main id="benefit">
+    <div class="container">
+      <h2 class="benefit-title">Benefit</h2>
+      <div class="benefit-box flex">
+        <div class="benefit-box-item">
+          <img src="asset/img/nox-product2.png" alt="" />
+        </div>
+        <div class="benefit-box-item">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+            nobis quasi asperiores in sequi soluta ipsa molestias voluptatem
+            ea, optio facere nisi cum rerum! Aspernatur consectetur sequi
+            commodi dicta vitae!
+          </p>
+        </div>
+      </div>
+    </div>
+  </main>
+  <!-- END BENEFIT -->
+
+  <!-- WHY -->
+  <main id="why">
+    <div class="container flex">
+      <div class="why-item flex">
+        <div class="why-row float-clear">
+          <div class="why-list float-l">1</div>
+          <p class="why-list-desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
+            excepturi, illum possimus est ab nulla ea maxime minima fuga amet
+            sapiente eos ut doloribus deleniti.
+          </p>
+        </div>
+        <div class="why-row float-clear">
+          <div class="why-list float-l">2</div>
+          <p class="why-list-desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
+            excepturi, illum possimus est ab nulla ea maxime minima fuga amet
+            sapiente eos ut doloribus deleniti.
+          </p>
+        </div>
+        <div class="why-row float-clear">
+          <div class="why-list float-l">3</div>
+          <p class="why-list-desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
+            excepturi, illum possimus est ab nulla ea maxime minima fuga amet
+            sapiente eos ut doloribus deleniti.
+          </p>
+        </div>
+      </div>
+      <div class="why-item">
+        <p class="whytitle-one">WHY</p>
+        <p class="whytitle-two">NOX SUPLEMENT</p>
+        <div class="whyimg flex">
+          <div class="whytitle-circle flex">
+            <img src="asset/img/nox-product2.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+  <!-- END WHY -->
+
+  <!-- FORMULA -->
+  <main id="formula">
+    <div class="container flex formula-wrap">
+      <div class="formula-item">
+        <div class="formula-circle">
+          <img src="asset/img/model.png" alt="" />
+        </div>
+      </div>
+      <div class="formula-item">
+        <h3 class="formula-title">Our Formula</h3>
+        <p class="formula-desc">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias sed
+          similique necessitatibus reprehenderit numquam atque harum, nostrum
+          animi recusandae vero enim natus! Possimus non expedita illum
+          similique maxime eum sunt placeat quibusdam? Lorem, ipsum dolor sit
+          amet consectetur adipisicing elit. Praesentium blanditiis earum
+          placeat similique tenetur consequuntur. Nihil ipsa exercitationem
+          earum ex!
+        </p>
+      </div>
+    </div>
+  </main>
+  <!-- END FORMULA -->
+
+  <!-- FEATURED  -->
+  <main id="featured">
+    <div class="container">
+      <h2 class="featured-title">FEATURED PRODUCTS</h2>
+      <div class="featured-row flex">
+        <div class="featured-item">
+          <div class="featured-circle">
+            <img src="asset/img/nox-product1.png" alt="" />
+          </div>
+          <p class="featured-product-title">CASEIN - Chocolate Flavor</p>
+          <p class="featured-product-pirce">$ 45.99</p>
+          <button class="btn">SHOP NOW</button>
+        </div>
+        <div class="featured-item">
+          <div class="featured-circle">
+            <img src="asset/img/nox-product1.png" alt="" />
+          </div>
+          <p class="featured-product-title">CASEIN - Chocolate Flavor</p>
+          <p class="featured-product-pirce">$ 45.99</p>
+          <button class="btn">SHOP NOW</button>
+        </div>
+        <div class="featured-item">
+          <div class="featured-circle">
+            <img src="asset/img/nox-product1.png" alt="" />
+          </div>
+          <p class="featured-product-title">CASEIN - Chocolate Flavor</p>
+          <p class="featured-product-pirce">$ 45.99</p>
+          <button class="btn">SHOP NOW</button>
+        </div>
+      </div>
+    </div>
+  </main>
+  <!-- END FEATURED  -->
+
+  <!-- BLOG -->
+  <main id="blog">
+    <div class="container">
+      <h2 class="blog-title">BLOG</h2>
+      <div class="blog-row flex">
+        <div class="blog-item">
+          <div class="relative">
+            <div class="blog-circle-date">
+              <p class="blog-d">20</p>
+              <p class="blog-m">Juni</p>
+              <p class="blog-y">2020</p>
+            </div>
+          </div>
+          <div class="blog-frame">
+            <img src="asset/img/blog1.jpg" alt="" />
+          </div>
+        </div>
+        <div class="blog-item">
+          <h3 class="blog-post-title">Blog Title Here</h3>
+          <p class="blog-post-desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+            praesentium, dolor voluptas neque porro pariatur iste natus
+            repellat voluptatum maxime, est temporibus corrupti culpa iure
+            doloribus veniam optio mollitia quae? Eveniet libero, sequi
+            blanditiis quia molestiae cum, modi repellendus iusto, natus
+            delectus sit placeat cumque!
+          </p>
+          <a href="">Details</a>
+        </div>
+      </div>
+      <div class="blog-row flex">
+        <div class="blog-item order-dua">
+          <div class="relative">
+            <div class="blog-circle-date">
+              <p class="blog-d">20</p>
+              <p class="blog-m">Juni</p>
+              <p class="blog-y">2020</p>
+            </div>
+          </div>
+          <div class="blog-frame">
+            <img src="asset/img/blog2.jpg" alt="" />
+          </div>
+        </div>
+        <div class="blog-item order-satu">
+          <h3 class="blog-post-title">Blog Title Here</h3>
+          <p class="blog-post-desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+            praesentium, dolor voluptas neque porro pariatur iste natus
+            repellat voluptatum maxime, est temporibus corrupti culpa iure
+            doloribus veniam optio mollitia quae? Eveniet libero, sequi
+            blanditiis quia molestiae cum, modi repellendus iusto, natus
+            delectus sit placeat cumque!
+          </p>
+          <a href="">Details</a>
+        </div>
+      </div>
+    </div>
+  </main>
+  <!-- END BLOG -->
+
+  <!-- SUBSCRIEB -->
+  <main id="subscribe">
+    <div class="container suscribe-wrap">
+      <p class="suscribe-title">Subscribe To Newsletter</p>
+      <form action="">
+        <div class="subscribe-form flex">
+          <input class="subs-email" type="text" placeholder="Enter Your Email Adress" />
+          <input class="subs-button" type="button" value="SUBSCRIBE" />
+        </div>
+      </form>
+    </div>
+  </main>
+  <!-- END SUBSCRIEB -->
+
+  <!-- TESTIMONI -->
+  <main id="testimony">
+    <div class="container">
+      <p class="testy-title">When Our Customer Say</p>
+      <div class="testy-row flex">
+        <div class="testy-card">
+          <img class="card-quote" src="asset/img/quote.svg" alt="" />
+          <p class="card-desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+            sapiente facilis esse voluptatum, incidunt reprehenderit eos totam
+            voluptates tempora autem laboriosam quam voluptatem distinctio
+            optio quas provident quo. Ad, laudantium?
+          </p>
+          <div class="card-line"></div>
+          <div class="card-profile">
+            <img src="asset/img/profile1.jpg" alt="" />
+          </div>
+          <p class="card-name">Michael Abraham</p>
+        </div>
+        <div class="testy-card">
+          <img class="card-quote" src="asset/img/quote.svg" alt="" />
+          <p class="card-desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+            sapiente facilis esse voluptatum, incidunt reprehenderit eos totam
+            voluptates tempora autem laboriosam quam voluptatem distinctio
+            optio quas provident quo. Ad, laudantium?
+          </p>
+          <div class="card-line"></div>
+          <div class="card-profile">
+            <img src="asset/img/profile2.jpg" alt="" />
+          </div>
+          <p class="card-name">Fionna Williamson</p>
+        </div>
+        <div class="testy-card">
+          <img class="card-quote" src="asset/img/quote.svg" alt="" />
+          <p class="card-desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+            sapiente facilis esse voluptatum, incidunt reprehenderit eos totam
+            voluptates tempora autem laboriosam quam voluptatem distinctio
+            optio quas provident quo. Ad, laudantium?
+          </p>
+          <div class="card-line"></div>
+          <div class="card-profile">
+            <img src="asset/img/profile3.jpg" alt="" />
+          </div>
+          <p class="card-name">Richard Miller</p>
+        </div>
+      </div>
+    </div>
+  </main>
+  <!-- END TESTIMONI -->
+
+  <!-- FOOTER -->
+  <footer>
+    <div class="container flex">
+      <div class="footer-item">
+        <h3 class="footer-title">Nox Supplement</h3>
+        <p class="footer-desc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
+          quibusdam facere ullam eveniet repellendus magnam autem, at sequi?
+          Ullam qui perspiciatis temporibus voluptate nostrum, eos cupiditate.
+          Nobis fuga ad quo?
+        </p>
+      </div>
+      <div class="footer-item flex">
+        <div class="footer-item-kanan">
+          <p>Navigate</p>
+          <a href="">Home</a>
+          <a href="">Product</a>
+          <a href="">Contact</a>
+          <a href="">About</a>
+        </div>
+        <div class="footer-item-kanan">
+          <p>Legal</p>
+          <a href="">Terms</a>
+          <a href="">Privacy</a>
+          <a href="">Cookies</a>
+          <a href="">Copyright</a>
+        </div>
+        <div class="footer-item-kanan">
+          <p>Contact Us</p>
+          <span>24/Suport </span>
+          <span>marketing@email.com </span>
+          <span>+1-800-1000-8888 </span>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- END FOOTER -->
+</body>
+
+</html>

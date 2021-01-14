489d5fb98e91ac2c1c8af65dc49fa0f6333ac0ce
faisalsaid <falsdesain@gmail.com>
2021-01-14 14:47:53 +0900

    fix portfolio title css

--- index.php
+++ index.php
@@ -1,314 +1,294 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <!-- boxicons libray  -->
-    <link
-      href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css"
-      rel="stylesheet"
-    />
-    <!-- css file -->
-    <link rel="stylesheet" href="asset/css/style.css" />
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <!-- gsap animation -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/CSSRulePlugin.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/ScrollTrigger.min.js"></script>
+  <!-- boxicons libray  -->
+  <link href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css" rel="stylesheet" />
+  <!-- css file -->
+  <link rel="stylesheet" href="asset/css/style.css" />
 
-    <!-- link menu scrool smoth -->
-    <script src="https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll@15.0.0/dist/smooth-scroll.polyfills.min.js"></script>
+  <!-- gsap animation -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/CSSRulePlugin.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/ScrollTrigger.min.js"></script>
 
-    <title>Bisadesign</title>
-  </head>
-  <body>
-    <!-- HEADER -->
-    <header id="home">
-      <nav class="main-nav">
-        <div class="container">
-          <div class="menu"><i class="bx bx-menu"></i></div>
-          <div class="logo">
-            <img src="asset/img/logo-bisadesign.svg" alt="" />
-            <p>bisadesign</p>
-          </div>
-          <ul class="list-menu">
-            <li><a href="#home">home</a></li>
-            <li>
-              <a href="#about">about</a>
-            </li>
-            <li>
-              <a href="#portfolios">portfolio</a>
-            </li>
-            <li>
-              <a href="#boxmail">contact</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+  <!-- link menu scrool smoth -->
+  <script src="https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll@15.0.0/dist/smooth-scroll.polyfills.min.js"></script>
+
+  <title>Bisadesign</title>
+</head>
+
+<body>
+  <!-- HEADER -->
+  <header id="home">
+    <nav class="main-nav">
       <div class="container">
-        <div class="hero">
-          <div class="hero-desc">
-            <h1>Just Focus To Grow The Business</h1>
-            <h1>WE DESIGN YOUR WEBSITE</h1>
-          </div>
-          <div class="hero-img">
-            <img
-              class="img-logo"
-              src="asset/img/bisadesain-logo-light.svg"
-              alt=""
-            />
-            <img src="asset/img/icon-wireframe.svg" alt="" class="img-icon" />
-            <img src="asset/img/icon-textedit.svg" alt="" class="img-icon" />
-            <img src="asset/img/icon-uicode.svg" alt="" class="img-icon" />
-            <img src="asset/img/icon-penedit.svg" alt="" class="img-icon" />
-          </div>
+        <div class="menu"><i class="bx bx-menu"></i></div>
+        <div class="logo">
+          <img src="asset/img/logo-bisadesign.svg" alt="" />
+          <p>bisadesign</p>
         </div>
-        <div class="social-media">
-          <span class="line"></span>
-          <ul>
-            <li>
-              <a href="https://instagram.com/bisadesign_id" target="_blank"
-                ><i class="bx bxl-instagram"></i
-              ></a>
-            </li>
-            <li>
-              <a href="https://behance.net/faisalsaid" target="_blank"
-                ><i class="bx bxl-behance"></i
-              ></a>
-            </li>
-            <li>
-              <a href="https://dribbble.com/faisalsaid" target="_blank"
-                ><i class="bx bxl-dribbble"></i
-              ></a>
-            </li>
-            <li>
-              <a href="mailto:bisadesain.ga@gmail.com"
-                ><i class="bx bx-mail-send"></i
-              ></a>
-            </li>
-          </ul>
+        <ul class="list-menu">
+          <li><a href="#home">home</a></li>
+          <li>
+            <a href="#about">about</a>
+          </li>
+          <li>
+            <a href="#portfolios">portfolio</a>
+          </li>
+          <li>
+            <a href="#boxmail">contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container">
+      <div class="hero">
+        <div class="hero-desc">
+          <h1>Just Focus To Grow The Business</h1>
+          <h1>WE DESIGN YOUR WEBSITE</h1>
+        </div>
+        <div class="hero-img">
+          <img class="img-logo" src="asset/img/bisadesain-logo-light.svg" alt="" />
+          <img src="asset/img/icon-wireframe.svg" alt="" class="img-icon" />
+          <img src="asset/img/icon-textedit.svg" alt="" class="img-icon" />
+          <img src="asset/img/icon-uicode.svg" alt="" class="img-icon" />
+          <img src="asset/img/icon-penedit.svg" alt="" class="img-icon" />
         </div>
       </div>
-    </header>
-    <!-- end HEADER -->
+      <div class="social-media">
+        <span class="line"></span>
+        <ul>
+          <li>
+            <a href="https://instagram.com/bisadesign_id" target="_blank"><i class="bx bxl-instagram"></i></a>
+          </li>
+          <li>
+            <a href="https://behance.net/faisalsaid" target="_blank"><i class="bx bxl-behance"></i></a>
+          </li>
+          <li>
+            <a href="https://dribbble.com/faisalsaid" target="_blank"><i class="bx bxl-dribbble"></i></a>
+          </li>
+          <li>
+            <a href="mailto:bisadesain.ga@gmail.com"><i class="bx bx-mail-send"></i></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </header>
+  <!-- end HEADER -->
 
-    <!-- ABOUT -->
-    <section id="about">
-      <div class="container">
-        <div class="about-wraper">
-          <h2 class="rubrik">about</h2>
-          <h2 class="rubrik-line">about</h2>
-          <div class="about-content-wrap">
-            <div class="about-img">
-              <div class="about-card">
-                <div class="circle">
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                </div>
-                <div class="square"></div>
-                <div class="dots">
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                </div>
-                <img src="asset/img/fals-profile.png" alt="" class="profile" />
+  <!-- ABOUT -->
+  <section id="about">
+    <div class="container">
+      <div class="about-wraper">
+        <h2 class="rubrik">about</h2>
+        <h2 class="rubrik-line">about</h2>
+        <div class="about-content-wrap">
+          <div class="about-img">
+            <div class="about-card">
+              <div class="circle">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
               </div>
+              <div class="square"></div>
+              <div class="dots">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+              </div>
+              <img src="asset/img/fals-profile.png" alt="" class="profile" />
             </div>
-            <div class="about-desc">
-              <h3>FAISAL SAID</h3>
-              <h4>UI/UX DESIGNER & Front End Programmer</h4>
-              <p>
-                Freelancer UI Designer and Front End Developer for 2 years,
-                having previously been in the Graphic Design with more than 8
-                years of experience.
-              </p>
-            </div>
+          </div>
+          <div class="about-desc">
+            <h3>FAISAL SAID</h3>
+            <h4>UI/UX DESIGNER & Front End Programmer</h4>
+            <p>
+              Freelancer UI Designer and Front End Developer for 2 years,
+              having previously been in the Graphic Design with more than 8
+              years of experience.
+            </p>
           </div>
         </div>
       </div>
-    </section>
-    <!-- end ABOUT -->
+    </div>
+  </section>
+  <!-- end ABOUT -->
 
-    <!-- portfolio -->
-    <section id="portfolio">
-      <div class="container">
-        <h2 id="portfolios" class="rubrik">portfolio</h2>
-        <h2 class="rubrik-line">portfolio</h2>
-      </div>
-      <div class="wraper-portfolios">
-        <div class="portfolio">
-          <p class="title container">NOX-Supplement</p>
-          <img src="asset/img/nox.jpg" alt="" />
-          <div class="container">
-            <a href="">Study Case <i class="bx bxs-right-arrow-alt"></i></a>
-          </div>
-        </div>
-        <div class="portfolio">
-          <p class="title container">Lean-envy</p>
-          <img src="asset/img/lean.jpg" alt="" />
-          <div class="container">
-            <a href="">Study Case <i class="bx bxs-right-arrow-alt"></i></a>
-          </div>
-        </div>
-        <div class="portfolio">
-          <p class="title container">Trademarking</p>
-          <img src="asset/img/trade.jpg" alt="" />
-          <div class="container">
-            <a href="">Study Case <i class="bx bxs-right-arrow-alt"></i></a>
-          </div>
+  <!-- portfolio -->
+  <section id="portfolio">
+    <div class="container">
+      <h2 id="portfolios" class="rubrik">portfolio</h2>
+      <h2 class="rubrik-line">portfolio</h2>
+    </div>
+    <div class="wraper-portfolios">
+      <div class="portfolio">
+        <p class="title container">NOX-Supplement</p>
+        <img src="asset/img/nox.jpg" alt="" />
+        <div class="container">
+          <a href="">Study Case <i class="bx bxs-right-arrow-alt"></i></a>
         </div>
       </div>
-    </section>
-    <!-- end portfolio -->
+      <div class="portfolio">
+        <p class="title container">Lean-envy</p>
+        <img src="asset/img/lean.jpg" alt="" />
+        <div class="container">
+          <a href="">Study Case <i class="bx bxs-right-arrow-alt"></i></a>
+        </div>
+      </div>
+      <div class="portfolio">
+        <p class="title container">Trademarking</p>
+        <img src="asset/img/trade.jpg" alt="" />
+        <div class="container">
+          <a href="">Study Case <i class="bx bxs-right-arrow-alt"></i></a>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end portfolio -->
 
-    <!-- email -->
-    <section id="boxmail">
-      <div class="container">
-        <p>Interested working with me?</p>
-        <a href="mailto: bisadesain.ga@gmail.com"
-          ><i class="bx bx-mail-send"></i> Send Me Email</a
-        >
-      </div>
-    </section>
-    <!-- end email -->
+  <!-- email -->
+  <section id="boxmail">
+    <div class="container">
+      <p>Interested working with me?</p>
+      <a href="mailto: bisadesain.ga@gmail.com"><i class="bx bx-mail-send"></i> Send Me Email</a>
+    </div>
+  </section>
+  <!-- end email -->
 
-    <!-- why and teckcno -->
-    <section id="why">
-      <div class="container">
-        <h2 class="rubrik">why me</h2>
-        <h2 class="rubrik-line">why_me</h2>
-        <div class="why-wraper">
-          <div class="cards">
-            <div class="card">
-              <i class="bx bx-comment-check"></i>
-              <h4>Communicative</h4>
-              <p>
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                diam nonumy eirmod tempor invidunt ut
-              </p>
-            </div>
-            <div class="card">
-              <i class="bx bx-git-compare"></i>
-              <h4>Collaborative​</h4>
-              <p>
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                diam nonumy eirmod tempor invidunt ut
-              </p>
-            </div>
-            <div class="card">
-              <i class="bx bxs-component"></i>
-              <h4>Management Freak</h4>
-              <p>
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                diam nonumy eirmod tempor invidunt ut
-              </p>
-            </div>
+  <!-- why and teckcno -->
+  <section id="why">
+    <div class="container">
+      <h2 class="rubrik">why me</h2>
+      <h2 class="rubrik-line">why_me</h2>
+      <div class="why-wraper">
+        <div class="cards">
+          <div class="card">
+            <i class="bx bx-comment-check"></i>
+            <h4>Communicative</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+              diam nonumy eirmod tempor invidunt ut
+            </p>
           </div>
-        </div>
-        <div class="technology">
-          <h4 class="rubrik">tools & technology</h4>
-          <div class="tools">
-            <div class="tool">
-              <img src="asset/img/logo-figma.png" alt="" />
-            </div>
-            <div class="tool">
-              <img src="asset/img/logo-xd.png" alt="" />
-            </div>
-            <div class="tool">
-              <img src="asset/img/logo-photo.png" alt="" />
-            </div>
-            <div class="tool">
-              <img src="asset/img/logo-ai.png" alt="" />
-            </div>
-            <div class="tool">
-              <img src="asset/img/logo-html.png" alt="" />
-            </div>
-            <div class="tool">
-              <img src="asset/img/logo-css.png" alt="" />
-            </div>
-            <div class="tool">
-              <img src="asset/img/logo-js.png" alt="" />
-            </div>
+          <div class="card">
+            <i class="bx bx-git-compare"></i>
+            <h4>Collaborative​</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+              diam nonumy eirmod tempor invidunt ut
+            </p>
+          </div>
+          <div class="card">
+            <i class="bx bxs-component"></i>
+            <h4>Management Freak</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+              diam nonumy eirmod tempor invidunt ut
+            </p>
           </div>
         </div>
       </div>
-    </section>
-    <!-- end why and teckcno -->
-
-    <!-- footer  -->
-    <footer>
-      <div class="container">
-        <h4>Get in touch</h4>
-        <p>
-          For business inquiry please send email to
-          <a href="mailto: bisadesain.ga@gmail.com">bisadesign@gmail.com</a>
-        </p>
-        <div class="social-media">
-          <ul>
-            <li>
-              <a href="https://instagram.com/bisadesign_id" target="_blank"
-                ><i class="bx bxl-instagram"></i
-              ></a>
-            </li>
-            <li>
-              <a href="https://behance.net/faisalsaid" target="_blank"
-                ><i class="bx bxl-behance"></i
-              ></a>
-            </li>
-            <li>
-              <a href="https://dribbble.com/faisalsaid" target="_blank"
-                ><i class="bx bxl-dribbble"></i
-              ></a>
-            </li>
-          </ul>
+      <div class="technology">
+        <h4 class="rubrik">tools & technology</h4>
+        <div class="tools">
+          <div class="tool">
+            <img src="asset/img/logo-figma.png" alt="" />
+          </div>
+          <div class="tool">
+            <img src="asset/img/logo-xd.png" alt="" />
+          </div>
+          <div class="tool">
+            <img src="asset/img/logo-photo.png" alt="" />
+          </div>
+          <div class="tool">
+            <img src="asset/img/logo-ai.png" alt="" />
+          </div>
+          <div class="tool">
+            <img src="asset/img/logo-html.png" alt="" />
+          </div>
+          <div class="tool">
+            <img src="asset/img/logo-css.png" alt="" />
+          </div>
+          <div class="tool">
+            <img src="asset/img/logo-js.png" alt="" />
+          </div>
         </div>
       </div>
-    </footer>
-    <!-- end footer  -->
+    </div>
+  </section>
+  <!-- end why and teckcno -->
 
-    <!-- local script -->
-    <script src="asset/js/scipt.js"></script>
-  </body>
+  <!-- footer  -->
+  <footer>
+    <div class="container">
+      <h4>Get in touch</h4>
+      <p>
+        For business inquiry please send email to
+        <a href="mailto: bisadesain.ga@gmail.com">bisadesign@gmail.com</a>
+      </p>
+      <div class="social-media">
+        <ul>
+          <li>
+            <a href="https://instagram.com/bisadesign_id" target="_blank"><i class="bx bxl-instagram"></i></a>
+          </li>
+          <li>
+            <a href="https://behance.net/faisalsaid" target="_blank"><i class="bx bxl-behance"></i></a>
+          </li>
+          <li>
+            <a href="https://dribbble.com/faisalsaid" target="_blank"><i class="bx bxl-dribbble"></i></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+  <!-- end footer  -->
+
+  <!-- local script -->
+  <script src="asset/js/scipt.js"></script>
+</body>
+
 </html>
